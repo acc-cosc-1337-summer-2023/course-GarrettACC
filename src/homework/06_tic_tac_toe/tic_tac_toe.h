90f011b7412043e2f0c6c1a063929c8956867d7b
GarrettACC <135080039+GarrettACC@users.noreply.github.com>
2023-07-15 23:04:18 +0000

    Homework 6: 4x4 Boards

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -2,12 +2,15 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<math.h>
+
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
 
 class TicTacToe
 {
 public:
+    TicTacToe(int size): pegs(size*size, " "){}
     bool game_over();
     void start_game(std::string first_player);
     void mark_board(int position);
@@ -15,16 +18,18 @@ public:
     void display_board() const;
     std::string get_winner() const{return winner;}
     void set_winner();
+protected:
+    std::vector<std::string> pegs;
+    virtual bool check_column_win() = 0;
+    virtual bool check_row_win() = 0;
+    virtual bool check_diagonal_win() = 0;
 private:
     std::string player;
-    std::vector<std::string> pegs{9, " "};
     void clear_board();
     void set_next_player();
     bool check_board_full()const;
     std::string winner;
-    bool check_column_win();
-    bool check_row_win();
-    bool check_diagonal_win();
+    
 
 };
 
